3784f895e876418a19ddc4be595a8a762efea142
Adriana Gizela <asampaio2002@gmail.com>
2025-11-06 16:07:27 +0100

    feat: layout para troca de tela e seus drawables adicionados

---
+++ app/src/main/java/ao/co/isptec/aplm/anunciosloc/activities/SettingsActivity.java
@@ -0,0 +1,15 @@
+package ao.co.isptec.aplm.anunciosloc.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import ao.co.isptec.aplm.anunciosloc.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
